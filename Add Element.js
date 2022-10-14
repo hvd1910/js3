b4f0c3536c33dd82a688dc677c58f17cf8b24b14
Hồ Việt Đức <112045195+shinducql@users.noreply.github.com>
2022-10-14 09:45:36 +0700

    comit
    comit

---
+++ Add Element.js
@@ -0,0 +1,9 @@
+var newEl = document.createElement('li');
+
+var newText = document.createElement('quinoa');
+
+newEl.appendChild(newText);
+
+var possition = document.getElementsByTagName('ul');
+
+possition.appendChild(newEl);
---
+++ query-selector.js
@@ -0,0 +1,5 @@
+var el = document.querySelectorAll('li.hot');
+el.className = ' cool';
+
+var els = document.querySelectorAll('li.hot');
+els[1].className = 'cool';
